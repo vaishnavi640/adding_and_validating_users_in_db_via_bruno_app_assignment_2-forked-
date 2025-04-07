ffc90d399773db2f36d122b8b62a74b1fcec0a76
vaishnavi640 <vaishnavikalluri@gmail.com>
2025-04-07 10:15:30 +0530

    changes done

--- index.js
+++ index.js
@@ -1,15 +1,73 @@
 const express = require('express');
-const { resolve } = require('path');
-
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');
+const User = require('./schema'); 
+require('dotenv').config();
 const app = express();
-const port = 3010;
+const port = 3000;
 
-app.use(express.static('static'));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Failed to connect to MongoDB', err);
+});
+
+app.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: 'Name, email, and password are required.' });
+  }
+
+  try {
+  
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists.' });
+    }
+
+    
+    const user = new User({ name, email, password });
+    await user.save();
+
+    res.status(201).json({ message: 'User registered successfully.' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error.' });
+  }
+});
+
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Email and password are required.' });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found.' });
+    }
+
+    // Compare 
+    if (user.password === password) {
+      return res.status(200).json({ message: 'Login successful.' });
+    } else {
+      return res.status(401).json({ message: 'Invalid credentials.' });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error.' });
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
